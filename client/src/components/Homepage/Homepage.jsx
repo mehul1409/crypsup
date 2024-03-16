1c19080a8f729afa87a2b65c007a45402a33e694
Shivanshu0512 <shivanshum512@gmail.com>
2024-03-16 12:34:04 +0530

    add css

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -6,13 +6,14 @@ const Homepage = () => {
   return (
 
     <div className="container">
+       <div class="ethereum-bg"></div>
       <div className="center-content">
-        <img src="icon.png" alt="CrypSup Icon" className="icon" />
+        <img src="/assets/crypsup.png" alt="CrypSup Icon" className="icon" />
         <h1 className="project-name">CrypSup</h1>
         <p className="description">A place where you get what you deserve</p>
         <div className="buttons">
           <Link to='/signup' className="button">Register as a Creator</Link>
-          <Link to='/supportersignup' className="button">Register as a Supporter</Link>
+          <Link to='/supportersignup' className='button'>Register as a Supporter</Link>
         </div>
       </div>
     </div>
